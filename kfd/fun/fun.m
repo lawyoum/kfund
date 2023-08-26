9b17f8341028ec11ccef0db697b45488bfc41846
wh1te4ever <88495487+wh1te4ever@users.noreply.github.com>
2023-08-26 23:13:24 +0900

    Fixed bootstrap, very early-access jb

--- kfd/fun/fun.m
+++ kfd/fun/fun.m
@@ -23,6 +23,7 @@
 #import "bootstrap.h"
 #import "boot_info.h"
 #import "jailbreakd_test.h"
+#import "helpers.h"
 
 void test_kalloc_kfree(void) {
     size_t allocated_size = 0x1000;
@@ -141,14 +142,14 @@ int do_fun(void) {
     loadTrustCacheBinaries();
     
     term_kcall();   //After term_kcall called, kalloc/kfree/physrw NOT work.
+//    goto TEMP_JUMP;
     
-    runSSH();
+    cleanDropbearBootstrap();
+//    runSSH(); //This would not be used anymore since have working JB.
     
     startJBEnvironment();   //oobPCI.swift -> case "startEnvironment":
-//    platformize(1); //orginally implemented from launchdhook
-    set_proc_csflags(1);
-    
-    
+
+//TEMP_JUMP:
 //    test_launchdhook();
     
 //    test_handoffKRW_jailbreakd();
@@ -156,6 +157,14 @@ int do_fun(void) {
 //    test_communicate_jailbreakd();
     
     sandbox(getpid(), sb);
+    
+    //jb done, kclose and sbreload
+    usleep(1000000);
+    do_kclose();
+    printf("Status: Done, sbreloading now...\n");
+    usleep(3000000);
+    restartBackboard();
+    
 
     return 0;
 }
